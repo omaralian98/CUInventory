c9a110209728ebf46a92f547c136f3f87a94cf21
Mr_Sure21 <52558137+omaralian98@users.noreply.github.com>
2026-07-11 12:35:17 +0300

    Removed the InventoryAdjustment aggregate

--- src/CUInventory.Domain/Inventory/Events/InventoryAdjustedDomainEvent.cs
+++
@@ -1,5 +0,0 @@
-using System;
-
-namespace CUInventory.Inventory.Events;
-
-public record InventoryAdjustedDomainEvent(Guid InventoryAdjustmentId, DateTime AdjustedAt);
--- src/CUInventory.Domain/Inventory/Repositories/IInventoryAdjustmentRepository.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Volo.Abp.Domain.Repositories;
-using CUInventory.Inventory.Aggregates;
-
-namespace CUInventory.Inventory.Repositories;
-
-public interface IInventoryAdjustmentRepository : IRepository<InventoryAdjustment, Guid>
-{
-}
